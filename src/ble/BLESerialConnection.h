656f3c0081e2b8d6717542d34d5d420494f1ef52
Henry Li <henry1943@163.com>
2024-10-14 08:50:02 +0800

    feat: connect to device without scanning

--- src/ble/BLESerialConnection.h
+++ src/ble/BLESerialConnection.h
@@ -15,24 +15,38 @@ class BLESerialConnection {
   bool isConnected() const { return _connected; }
 
   bool connect(const char* deviceAddressLiteral) {
-    NimBLEAddress deviceAddress(deviceAddressLiteral);
     NimBLEDevice::init("");
     NimBLEDevice::setPower(esp_power_level_t::ESP_PWR_LVL_P9);
-    NimBLEScan* scan = NimBLEDevice::getScan();
-    scan->setActiveScan(true);
-    scan->setInterval(45);
-    scan->setWindow(15);
-    log_i("Scanning BLE devices for '%s'...", deviceAddressLiteral);
-    auto results = scan->start(3);
-    for (int i = 0; i < results.getCount(); i++) {
-      NimBLEAdvertisedDevice advertisedDevice = results.getDevice(i);
-      if (advertisedDevice.getAddress().equals(deviceAddress)) {
-        log_i("BLE device found");
-        onDeviceFound(&advertisedDevice);
-        return isConnected();
+
+    log_i("Connecting to BLE device...");
+    NimBLEAddress deviceAddress(deviceAddressLiteral, 1);
+    _client = NimBLEDevice::createClient(deviceAddress);
+    if (!_client->connect()) {
+      log_e("Failed to connect to BLE device");
+      return false;
+    }
+    log_i("Connected to BLE device");
+
+    auto services = _client->getServices(true);
+    for (int i = 0; i < services->size(); i++) {
+      auto service = services->at(i);
+      if (service->getUUID().equals(SERIAL_SERVICE_ID)) {
+        log_i("BLE serial service found");
+        auto characteristics = service->getCharacteristics(true);
+        for (int j = 0; j < characteristics->size(); j++) {
+          auto characteristic = characteristics->at(j);
+          if (characteristic->getUUID().equals(SERIAL_CHARACTERISTIC_ID)) {
+            log_i("BLE serial characteristic found");
+            _characteristic = characteristic;
+            log_i("handler %d", _characteristic->getHandle());
+            _connected = true;
+            log_i("BLE serial connection is now ready to interact");
+            return true;
+          }
+        }
       }
     }
-    log_e("BLE device not found");
+    log_e("BLE serial service not found");
     return false;
   }
 
@@ -54,37 +68,6 @@ class BLESerialConnection {
       _stream->subscribe();
     }
     return _stream;
-  }
-
- protected:
-  void onDeviceFound(NimBLEAdvertisedDevice* advertisedDevice) {
-    log_i("Connecting to BLE device...");
-    _client = NimBLEDevice::createClient();
-    if (!_client->connect(advertisedDevice)) {
-      log_e("Failed to connect to BLE device");
-      return;
-    }
-    log_i("Connected to BLE device");
-
-    auto services = _client->getServices(true);
-    for (int i = 0; i < services->size(); i++) {
-      auto service = services->at(i);
-      if (service->getUUID().equals(SERIAL_SERVICE_ID)) {
-        log_i("BLE serial service found");
-        auto characteristics = service->getCharacteristics(true);
-        for (int j = 0; j < characteristics->size(); j++) {
-          auto characteristic = characteristics->at(j);
-          if (characteristic->getUUID().equals(SERIAL_CHARACTERISTIC_ID)) {
-            log_i("BLE serial characteristic found");
-            _characteristic = characteristic;
-            _connected = true;
-            log_i("BLE serial connection is now ready to interact");
-            return;
-          }
-        }
-      }
-    }
-    log_e("BLE serial service not found");
   }
 
  private:
